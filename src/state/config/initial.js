cd8cf6815f18c82ac06d79b80c176d0764fd13e9
John Godley <johng75@gmail.com>
2017-03-03 12:32:40 +0000

    Detect if kdesudo is installed

--- src/state/config/initial.js
+++ src/state/config/initial.js
@@ -1,13 +1,34 @@
 /**
+ * External dependencies
+ */
+
+import fs from 'fs';
+import os from 'os';
+
+/**
  * Internal dependencies
  */
 
-import { STAGE_INTRO } from './type';
+import { STAGE_INTRO, STAGE_SHOW_SUDOPROMPT } from './type';
 import { getDefaultSettings } from './selector';
+
+function hasSudoPrompt() {
+	const paths = [ '/usr/bin/kdesudo' ];
+
+	return paths.some( item => fs.existsSync( item ) );
+}
+
+function getStartStage() {
+	if ( os.platform() === 'linux' && ! hasSudoPrompt() ) {
+		return STAGE_SHOW_SUDOPROMPT;
+	}
+
+	return STAGE_INTRO;
+}
 
 export function getInitialConfig() {
 	return {
-		stage: STAGE_INTRO,
+		stage: getStartStage(),
 		validity: [],
 		settings: getDefaultSettings(),
 	};
